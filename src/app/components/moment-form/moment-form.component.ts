c45b8bb1cf13d1f2b97341b23893cf250920e286
RogerXavier <rogerxavier10@gmail.com>
2023-05-26 10:16:10 -0300

    posting data using FormData

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { Moment } from 'src/app/Moment';
 
 @Component({
   selector: 'app-moment-form',
@@ -7,6 +9,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./moment-form.component.css'],
 })
 export class MomentFormComponent {
+  @Output() onSubmit = new EventEmitter<Moment>();
   @Input() btnText!: string;
 
   momentForm!: FormGroup;
@@ -28,9 +31,14 @@ export class MomentFormComponent {
     return this.momentForm.get('description')!;
   }
 
-  onSubmit() {
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+    this.momentForm.patchValue({ image: file });
+  }
+
+  submit() {
     if (this.momentForm.invalid) return;
 
-    console.log('submited');
+    this.onSubmit.emit(this.momentForm.value);
   }
 }
--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+import { Moment } from 'src/app/Moment';
+import { MomentService } from 'src/app/services/moment/moment.service';
+
 @Component({
   selector: 'app-new-moment',
   templateUrl: './new-moment.component.html',
   styleUrls: ['./new-moment.component.css'],
 })
 export class NewMomentComponent {
+  constructor(private momentService: MomentService) {}
   btnText: string = 'Share!';
+
+  async createHandler(moment: Moment) {
+    const formData = new FormData();
+
+    formData.append('title', moment.title);
+    formData.append('description', moment.description);
+    if (moment.image) {
+      formData.append('image', moment.image);
+    }
+
+    await this.momentService.createMoment(formData).subscribe();
+  }
 }
